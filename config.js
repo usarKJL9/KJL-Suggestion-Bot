bf5f48662dbb4c721d202dd232b725c1f0c16b1f
usarKJL9 <fivekjl@gmail.com>
2026-05-12 19:33:24 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const Config = {
+    TOKEN: "YOUR_BOT_TOKEN",
+    Suggestions_CHANNEL_ID: "123456789",
+    Feedback_CHANNEL_ID: "123456789",
+    Admin_ROLE_ID: "123456789"
+};
+
+module.exports = Config;
